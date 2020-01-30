c059dd6e1e31e25e9e32e451d4cfa4339ed94081
Matrix <1248540980@qq.com>
2020-01-30 16:25:03 +0800

    first commit

---
+++ src/UnionFound/IUnionFound.java
@@ -0,0 +1,41 @@
+package UnionFound;
+
+/**
+ * @author 刘建雯
+ */
+public interface IUnionFound {
+    /**
+     * 将p节点与q节点进行连接
+     * @param p 连接的第一个节点
+     * @param q 连接的第二个节点
+     */
+    void union(int p, int q);
+
+    /**
+     * 查找指定序号p所在分量的标识符
+     * @param p 指定序号p
+     * @return  返回所在分量的标识符
+     */
+    int find(int p);
+
+    /**
+     * 查找序号p与序号q是否属于同一连通分量
+     * @param p 第一个节点
+     * @param q 第二个节点
+     * @return  如果属于同一连通分量，则返回true
+     *          如果不属于同一连通分量，则返回false
+     */
+    boolean connected(int p,int q);
+
+    /**
+     * 获得当前连通分量的数量
+     * @return 连通分量的数量
+     */
+    int count();
+
+    /**
+     * 将固定配置文件进行读取，并进行连通
+     * 最终所有节点都按照配置文件所规定连接
+     */
+    void sort();
+}
